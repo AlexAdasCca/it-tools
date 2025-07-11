4f413cd8d0e64dba0086517d761ee99704259052
CthRio <3500197013@qq.com>
2025-07-11 13:48:20 +0800

    尝试进一步使用 I18n

--- src/tools/hash-text/hash-text.vue
+++ src/tools/hash-text/hash-text.vue
@@ -37,7 +37,7 @@ const hashText = (algo: AlgoNames, value: string) => formatWithEncoding(algos[al
 <template>
   <div>
     <c-card>
-      <c-input-text v-model:value="clearText" multiline raw-text placeholder="Your string to hash..." rows="3" autosize autofocus label="Your text to hash:" />
+      <c-input-text v-model:value="clearText" multiline raw-text placeholder="$t('hash-text.input.placeholder')" rows="3" autosize autofocus label="$t('hash-text.input.label')" />
 
       <n-divider />
 
