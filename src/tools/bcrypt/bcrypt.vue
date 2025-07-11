fb8952b3310cac7c44c2e376fced7b6409405b1c
CthRio <3500197013@qq.com>
2025-07-11 17:09:49 +0800

    fix: 修复奇怪的 s.$s

--- src/tools/bcrypt/bcrypt.vue
+++ src/tools/bcrypt/bcrypt.vue
@@ -10,7 +10,8 @@ const saltCount = ref(10);
 const hashed = computed(() => hashSync(input.value, saltCount.value));
 
 const { t } = useI18n()
-const { copy } = useCopy({ source: hashed, text: t('tools.bcrypt-card.copy-tooltip') });
+const $s = t // 给 `$s` 定义别名
+const { copy } = useCopy({ source: hashed, text: $s('tools.bcrypt-card.copy-tooltip') });
 
 const compareString = ref('');
 const compareHash = ref('');
@@ -18,39 +19,39 @@ const compareMatch = computed(() => compareSync(compareString.value, compareHash
 </script>
 
 <template>
-  <c-card :title="$s('tools.bcrypt-card.title')">
+  <c-card :title="('tools.bcrypt-card.title')">
     <c-input-text
       v-model:value="input"
-      :placeholder="$s('tools.bcrypt-card.in-bcrypt')"
+      :placeholder="('tools.bcrypt-card.in-bcrypt')"
       raw-text
-      :label="$s('tools.bcrypt-card.label-string')"
+      :label="('tools.bcrypt-card.label-string')"
       label-position="left"
       label-align="right"
       label-width="120px"
       mb-2
     />
-    <n-form-item :label="$s('tools.bcrypt-card.label-salt-count')" label-placement="left" label-width="120">
-      <n-input-number v-model:value="saltCount" :placeholder="$s('tools.bcrypt-card.label-salt-round')" :max="100" :min="0" w-full />
+    <n-form-item :label="('tools.bcrypt-card.label-salt-count')" label-placement="left" label-width="120">
+      <n-input-number v-model:value="saltCount" :placeholder="('tools.bcrypt-card.label-salt-round')" :max="100" :min="0" w-full />
     </n-form-item>
 
     <c-input-text :value="hashed" readonly text-center />
 
     <div mt-5 flex justify-center>
       <c-button @click="copy()">
-        {{ $s('tools.bcrypt-card.copy-button') }}
+        {{ ('tools.bcrypt-card.copy-button') }}
       </c-button>
     </div>
   </c-card>
 
-  <c-card :title="$s('tools.bcrypt-card.comp-title')">
+  <c-card :title="('tools.bcrypt-card.comp-title')">
     <n-form label-width="120">
-      <n-form-item :label="$s('tools.bcrypt-card.label-string')" label-placement="left">
-        <c-input-text v-model:value="compareString" :placeholder="$s('tools.bcrypt-card.comp-string')" raw-text />
+      <n-form-item :label="('tools.bcrypt-card.label-string')" label-placement="left">
+        <c-input-text v-model:value="compareString" :placeholder="('tools.bcrypt-card.comp-string')" raw-text />
       </n-form-item>
       <n-form-item label="Your hash: " label-placement="left">
-        <c-input-text v-model:value="compareHash" :placeholder="$s('tools.bcrypt-card.comp-hash')" raw-text />
+        <c-input-text v-model:value="compareHash" :placeholder="('tools.bcrypt-card.comp-hash')" raw-text />
       </n-form-item>
-      <n-form-item :label="$s('tools.bcrypt-card.hash-matched')" label-placement="left" :show-feedback="false">
+      <n-form-item :label="('tools.bcrypt-card.hash-matched')" label-placement="left" :show-feedback="false">
         <div class="compare-result" :class="{ positive: compareMatch }">
           {{ compareMatch ? 'Yes' : 'No' }}
         </div>
